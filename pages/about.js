680bf70da1d6a0731d94c73fcf5ea34ff2941875
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-10-07 17:08:48 +0900

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function about() {
+	return (
+		<div>
+			<h1>About</h1>
+			<p>This is an app to find EVENTS!</p>
+		</div>
+	);
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventPage() {
+	return (
+		<div>
+			<h1>My Event 2</h1>
+		</div>
+	);
+}
