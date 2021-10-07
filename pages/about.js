6420714533bdc8b5f17dfd8b1c92bac8bcd86206
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-10-07 17:52:55 +0900

    Layout, Header and Footer

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
-export default function about() {
+import Layout from '../components/Layout';
+
+export default function AboutPage() {
 	return (
-		<div>
+		<Layout title='Blah blah Title'>
 			<h1>About</h1>
 			<p>This is an app to find EVENTS!</p>
-		</div>
+		</Layout>
 	);
 }
